831b81de38231fdf86b1d72f07ea5fe7990a1886
Esin <nbusra.esin@gmail.com>
2023-06-12 02:09:05 +0300

    done

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About({src="https://via.placeholder.com/215" , text}){
+    return(
+        <aside>
+            <img src={src} alt="blog logo" ></img>
+            <p>{text}</p>
+        </aside>
+    )
+}
+
+export default About;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+
+import Article from "./Article";
+
+function ArticleList({posts}){
+
+    return(
+        <main>
+            {posts.map((post) => {
+                return <Article key={post.id} post={post}/>
+            } )}
+        </main>
+
+    )
+}
+
+export default ArticleList;
